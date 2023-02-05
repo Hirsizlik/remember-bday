5f0fd3f9da59944f9f69420e51cdfa3bde19832a
Markus Schagerl <Markus.Schagerl1@gmx.at>
2023-02-05 13:13:20 +0100

    add tests for send_bday_notifications

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,10 @@
 pub mod notifications;
 pub mod vcard;
 
-use std::fmt;
-use std::error;
-use vcard::VCard;
 use chrono::prelude::*;
+use std::error;
+use std::fmt;
+use vcard::VCard;
 
 pub struct Config {
     pub file_path: String,
@@ -29,7 +29,7 @@ pub trait Notifier {
 
 #[derive(Debug)]
 pub struct NotifierError {
-    message: Box<str>
+    message: Box<str>,
 }
 
 impl error::Error for NotifierError {}
@@ -40,17 +40,18 @@ impl fmt::Display for NotifierError {
     }
 }
 
-pub fn send_bday_notifications(notifier: &dyn Notifier, vcards: Vec<VCard>) -> Result<(), NotifierError> {
-    // TODO refactor to trait for connection/proxy + Tests
+pub fn send_bday_notifications(
+    notifier: &dyn Notifier,
+    vcards: Vec<VCard>,
+) -> Result<(), NotifierError> {
+    // TODO Tests
 
     let today = chrono::Local::now().date_naive();
     for vcard in vcards {
         if let Some(bday) = vcard.bday {
             if bday.month() == today.month() && bday.day() == today.day() {
                 // TODO localization?
-                notifier.send_notification(
-                    format!("It's {}'s birthday today!", vcard.name),
-                )?;
+                notifier.send_notification(format!("It's {}'s birthday today!", vcard.name))?;
             }
         }
     }
@@ -58,23 +59,10 @@ pub fn send_bday_notifications(notifier: &dyn Notifier, vcards: Vec<VCard>) -> R
     Ok(())
 }
 
-/*
-fn send_notification(proxy: &Proxy<&Connection>, message: String) -> Result<(), dbus::Error> {
-    let mut map = PropMap::new();
-    map.insert(
-        String::from("title"),
-        Variant(Box::new(String::from("Remember B-Day"))),
-    );
-    map.insert(String::from("body"), Variant(Box::new(message)));
-    // TODO icon (cake)
-    proxy.add_notification("my-id", map)?;
-    Ok(())
-}
-*/
-
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::cell::RefCell;
 
     #[test]
     fn build_config_success() {
@@ -95,5 +83,78 @@ mod tests {
         let test_args = vec!["ignored", "/a/path/to/a.txt", "also ignored"];
         let config = Config::build(test_args.iter().map(|s| String::from(*s)));
         assert!(config.is_err());
+    }
+
+    struct MockNotifier {
+        messages: RefCell<Vec<String>>,
+    }
+
+    impl<'a> Notifier for MockNotifier {
+        fn send_notification(&self, message: String) -> Result<(), NotifierError> {
+            self.messages.borrow_mut().push(message);
+            Ok(())
+        }
+    }
+
+    impl MockNotifier {
+        fn new() -> MockNotifier {
+            MockNotifier {
+                messages: RefCell::new(Vec::new()),
+            }
+        }
+    }
+
+    #[test]
+    fn send_bday_notifications_none() {
+        let mn = MockNotifier::new();
+
+        // either yesterday or tomorrow, whichever works
+        let not_today = chrono::Local::now()
+            .date_naive()
+            .checked_add_days(chrono::Days::new(1))
+            .or_else(|| {
+                Some(
+                    chrono::Local::now()
+                        .date_naive()
+                        .checked_sub_days(chrono::Days::new(1))
+                        .unwrap(),
+                )
+            });
+        let vcards = vec![
+            VCard {
+                name: "Test No Birthday".to_string(),
+                bday: None,
+            },
+            VCard {
+                name: "Test Birthday not today".to_string(),
+                bday: not_today,
+            },
+        ];
+        send_bday_notifications(&mn, vcards).unwrap();
+        assert_eq!(Vec::<String>::new(), mn.messages.into_inner());
+    }
+
+    #[test]
+    fn send_bday_notifications_multiple() {
+        let mn = MockNotifier::new();
+        let today = chrono::Local::now().date_naive();
+        let vcards = vec![
+            VCard {
+                name: "Test 1".to_string(),
+                bday: Some(today),
+            },
+            VCard {
+                name: "Test 2".to_string(),
+                bday: Some(today),
+            },
+        ];
+        send_bday_notifications(&mn, vcards).unwrap();
+        assert_eq!(
+            vec![
+                "It's Test 1's birthday today!".to_string(),
+                "It's Test 2's birthday today!".to_string()
+            ],
+            mn.messages.into_inner()
+        );
     }
 }
